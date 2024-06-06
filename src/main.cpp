47a6f99f54386eb6271200fc0d4352f36b178636
Heller0106 <bagit963@gmail.com>
2024-06-06 14:08:05 +0100

    working cam see whatsapp

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,53 @@
-#include <Arduino.h>
-#include "WiFi.h"
 #include "esp_camera.h"
-#include "esp_timer.h"
-#include "img_converters.h"
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
-#include "driver/rtc_io.h"
-#include <SPIFFS.h>
-#include <FS.h>
+#include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
+#include <esp_http_server.h>
 
+// Select camera model
+#define CAMERA_MODEL_AI_THINKER
+
+#include "camera_pins.h"
+
+// Flash
+#define LED_BUILTIN 4
+
+// WIFI config
 const char *ssid = "LAPTOP-INL7NVL4 4267";
 const char *password = "023=26xC";
 
-const char *mqtt_server = "wouterpeetermans.com";
-const int mqtt_port = 1884;
-const char *mqtt_user = "YOUR_MQTT_USERNAME";     // if applicable
-const char *mqtt_password = "YOUR_MQTT_PASSWORD"; // if applicable
-const char *mqtt_topic = "cm/picture";
+// MQTT config
+bool useMQTT = true;
+const char *mqttServer = "wouterpeetermans.com";
+const char *HostName = "Photobooth_Scaleway";
+const char *mqttUser = "your-device-id";
+const char *mqttPassword = "";
+const char *topic_PHOTO = "CM1/PICTURE";
+const char *topic_PUBLISH = "CM1/CAM";
+const char *topic_FLASH = "CM1/FLASH";
+const int MAX_PAYLOAD = 60000;
+
+bool flash;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#define PWDN_GPIO_NUM 31
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
-#define photo_path "/image.jpg"
-#define MAX_MQTT_PACKET_SIZE 128 // 128 bytes per chunk
+void startCameraServer();
+void callback(char* topic, byte* message, unsigned int length);
+void take_picture();
+void set_flash();
+void sendMQTT(const uint8_t *buf, uint32_t len);
 
-#define BUTTON_PIN 4 // Change this to the pin your button is connected to
+void setup() {
+    // Define Flash as an output
+    pinMode(LED_BUILTIN, OUTPUT);
 
-void setup_camera();
-void connect_wifi();
-void connect_mqtt();
-void capture_and_send_photo();
-bool check_photo(fs::FS &fs);
-void capture_save_photo(void);
-void send_mqtt_chunks(uint8_t *data, size_t len);
-void setup_button();
-void check_button();
-
-void setup()
-{
+    // Initialise the Serial Communication
     Serial.begin(115200);
-    
-    setup_button(); // Initialize button
-    
-    connect_wifi();
-    
-    if (!SPIFFS.begin(true))
-    {
-        Serial.println("An Error has occurred while mounting SPIFFS");
-        ESP.restart();
-    }
-    else
-    {
-        delay(500);
-        Serial.println("SPIFFS mounted successfully");
-    }
-    
-    setup_camera();
-    
-    client.setServer(mqtt_server, mqtt_port);
-    connect_mqtt();
-}
+    Serial.setDebugOutput(true);
+    Serial.println();
 
-void loop()
-{
-    if (!client.connected())
-    {
-        connect_mqtt();
-    }
-    client.loop();
-    
-    check_button(); // Check button state
-}
-
-void setup_camera()
-{
+    // Config Camera Settings
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
     config.ledc_timer = LEDC_TIMER_0;
@@ -113,160 +69,157 @@ void setup_camera()
     config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
     config.pixel_format = PIXFORMAT_JPEG;
-    // Reduce the resolution to ensure smaller size images
-    config.frame_size = FRAMESIZE_QVGA; // QVGA is 320x240 pixels
-    config.jpeg_quality = 10;           // Higher number means lower quality
-    config.fb_count = 1;
+
+    if (psramFound()) {
+        config.frame_size = FRAMESIZE_UXGA;
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+    } else {
+        config.frame_size = FRAMESIZE_SVGA;
+        config.jpeg_quality = 12;
+        config.fb_count = 1;
+    }
+
+    flash = true;
+
+    // camera init
     esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK)
-    {
+    if (err != ESP_OK) {
         Serial.printf("Camera init failed with error 0x%x", err);
-        ESP.restart();
+        return;
     }
-}
 
-void connect_wifi()
-{
+    sensor_t *s = esp_camera_sensor_get();
+    if (s->id.PID == OV3660_PID) {
+        s->set_vflip(s, 1);       // flip it back
+        s->set_brightness(s, 1);  // up the brightness just a bit
+        s->set_saturation(s, -2); // lower the saturation
+    }
+    s->set_framesize(s, FRAMESIZE_QVGA);
+
     WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    startCameraServer();
+
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+
+    client.setServer(mqttServer, 1884);
+    client.setBufferSize(MAX_PAYLOAD); // This is the maximum payload length
+    client.setCallback(callback);
 }
 
-void connect_mqtt()
-{
-    while (!client.connected())
-    {
-        Serial.println("Connecting to MQTT...");
-        if (client.connect("ESP32CAMClient", mqtt_user, mqtt_password))
-        {
-            Serial.println("connected");
-        }
-        else
-        {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
-            delay(2000);
-        }
+void callback(char* topic, byte* message, unsigned int length) {
+    String messageTemp;
+    Serial.println(topic);
+    for (int i = 0; i < length; i++) {
+        messageTemp += (char)message[i];
     }
-}
-
-void capture_and_send_photo()
-{
-    capture_save_photo();
-    
-    if (check_photo(SPIFFS))
-    {
-        File photoFile = SPIFFS.open(photo_path, FILE_READ);
-        
-        if (photoFile)
-        {
-            size_t photoSize = photoFile.size();
-            uint8_t *photoBuffer = (uint8_t *)malloc(photoSize);
-            photoFile.read(photoBuffer, photoSize);
-            photoFile.close();
-            send_mqtt_chunks(photoBuffer, photoSize);
-            free(photoBuffer);
-        }
-        else
-        {
-            Serial.println("Failed to open photo file");
-        }
+    if (String(topic) == topic_PHOTO) {
+        take_picture();
     }
-    else
-    {
-        Serial.println("Failed to capture photo");
+    if (String(topic) == topic_FLASH) {
+        set_flash();
     }
 }
 
-void send_mqtt_chunks(uint8_t *data, size_t len)
-{
-    size_t chunkSize = MAX_MQTT_PACKET_SIZE;
-    size_t offset = 0;
-    while (offset < len)
-    {
-        size_t remaining = len - offset;
-        if (remaining < chunkSize)
-        {
-            chunkSize = remaining;
-        }
-        if (client.publish(mqtt_topic, data + offset, chunkSize))
-        {
-            Serial.printf("Sent chunk: %d bytes\n", chunkSize);
-        }
-        else
-        {
-            Serial.println("Failed to send chunk to MQTT");
-        }
-        offset += chunkSize;
-        delay(100); // Short delay to avoid flooding the broker
-    }
-}
-
-bool check_photo(fs::FS &fs)
-{
-    File f_pic = fs.open(photo_path);
-    unsigned int pic_sz = f_pic.size();
-    f_pic.close();
-    return (pic_sz > 100);
-}
-
-void capture_save_photo(void)
-{
+void take_picture() {
     camera_fb_t *fb = NULL;
-    bool ok = 0;
-    do
-    {
-        Serial.println("ESP32-CAM capturing photo...");
-        fb = esp_camera_fb_get();
-        if (!fb)
-        {
-            Serial.println("Failed to capture photo");
-            return;
-        }
-        File file = SPIFFS.open(photo_path, FILE_WRITE);
-        if (!file)
-        {
-            Serial.println("Failed to open file in writing mode");
-        }
-        else
-        {
-            file.write(fb->buf, fb->len);
-            Serial.print("Photo saved in ");
-            Serial.print(photo_path);
-            Serial.print(" - Size: ");
-            Serial.print(file.size());
-            Serial.println("bytes");
-        }
-        file.close();
-        esp_camera_fb_return(fb);
-        ok = check_photo(SPIFFS);
-    } while (!ok);
-}
-
-void setup_button()
-{
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-}
-
-void check_button()
-{
-    static bool buttonWasPressed = false;
-    bool buttonState = digitalRead(BUTTON_PIN);
-    
-    if (buttonState == LOW && !buttonWasPressed)
-    {
-        // Button pressed, trigger capture
-        capture_and_send_photo();
-        buttonWasPressed = true;
+    if (flash) {
+        digitalWrite(LED_BUILTIN, HIGH);
+    };
+    Serial.println("Taking picture");
+    fb = esp_camera_fb_get(); // used to get a single picture.
+    if (!fb) {
+        Serial.println("Camera capture failed");
+        return;
     }
-    else if (buttonState == HIGH && buttonWasPressed)
-    {
-        // Button released, reset button press flag
-        buttonWasPressed = false;
+    Serial.println("Picture taken");
+    digitalWrite(LED_BUILTIN, LOW);
+    sendMQTT(fb->buf, fb->len);
+    esp_camera_fb_return(fb); // must be used to free the memory allocated by esp_camera_fb_get().
+}
+
+void set_flash() {
+    flash = !flash;
+    Serial.print("Setting flash to ");
+    Serial.println(flash);
+    if (!flash) {
+        for (int i = 0; i < 6; i++) {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+        }
+    }
+    if (flash) {
+        for (int i = 0; i < 3; i++) {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(500);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(100);
+        }
+    }
+}
+
+void sendMQTT(const uint8_t *buf, uint32_t len) {
+    Serial.println("Sending picture...");
+    if (len > MAX_PAYLOAD) {
+        Serial.print("Picture too large, increase the MAX_PAYLOAD value");
+    } else {
+        Serial.print("Picture sent ? : ");
+        Serial.println(client.publish(topic_PUBLISH, buf, len, false));
+    }
+}
+
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect(HostName, mqttUser, mqttPassword)) {
+            Serial.println("connected");
+            client.subscribe(topic_PHOTO);
+            client.subscribe(topic_FLASH);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+void loop() {
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+}
+
+// HTTP Server code
+static esp_err_t index_handler(httpd_req_t *req) {
+    httpd_resp_send(req, "ESP32 Camera Web Server", HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static const httpd_uri_t index_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = index_handler,
+    .user_ctx  = NULL
+};
+
+void startCameraServer() {
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    httpd_handle_t server = NULL;
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &index_uri);
     }
 }
